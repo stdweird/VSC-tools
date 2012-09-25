f67052633dfee37a03c7d26905d4104d732639a2
stdweird <stijn.deweirdt@ugent.be>
2012-09-26 00:18:51 +0200

    setuptools / easy_install support

--- setup.py
+++ setup.py
@@ -64,7 +64,17 @@ TODO:
     create http://hpcugent.github.com/VSC-tools page, short description per package ?
 
 """
-import distutils.command.install_scripts
+try:
+    ## setuptools makes copies of the scripts, does nto preserve symlinks
+    from setuptools import setup
+    from setuptools.command.install_scripts import install_scripts
+    from setuptools.command.easy_install import easy_install
+except:
+    from distutils.core import setup
+    from distutils.command.install_scripts import install_scripts
+    easy_install = object
+
+from distutils import log  # also for setuptools
 import shutil
 import os
 
@@ -72,17 +82,35 @@ import os
 FAKE_SUBDIRECTORY_NAME = 'fake'
 MYMPIRUN_ALIASES = ['%smpirun' % x for x in ['i', 'ih', 'o', 'm', 'mh', 'mm', 'q', 'm2', 'm2h']]
 
-class vsc_install_scripts(distutils.command.install_scripts.install_scripts):
+class vsc_easy_install(easy_install):
+    def install_egg_scripts(self, dist):
+        orig_func = dist.metadata_listdir
+        def new_func(txt):
+            """The original metadat_listdir assumes no subdirectories in scripts dir.
+                fake/mpirun is the exception (mpirun itself is not listed !)
+            """
+            res = orig_func(txt)
+            if txt == 'scripts':
+                idx = res.index(FAKE_SUBDIRECTORY_NAME)
+                if idx >= 0:
+                    res[idx] = '%s/mpirun' % FAKE_SUBDIRECTORY_NAME
+            return res
+        dist.metadata_listdir = new_func
+        easy_install.install_egg_scripts(self, dist)
+
+
+class vsc_install_scripts(install_scripts):
     """Create the (fake) links for mympirun
         also remove .sh and .py extensions from the scripts
     """
     def __init__(self, *args):
-        distutils.command.install_scripts.install_scripts.__init__(self, *args)
+        install_scripts.__init__(self, *args)
         self.original_outfiles = None
 
     def run(self):
         ## old-style class
-        distutils.command.install_scripts.install_scripts.run(self)
+        install_scripts.run(self)
+
         self.original_outfiles = self.get_outputs()[:] ## make a copy
         self.outfiles = [] ## reset it
         for script in self.original_outfiles:
@@ -94,9 +122,7 @@ class vsc_install_scripts(distutils.command.install_scripts.install_scripts):
 
             if script.endswith('/mympirun'):
                 ## make the fake dir, create all symlinks
-                abs_fakepath = os.path.join(self.install_dir, FAKE_SUBDIRECTORY_NAME)
-                if not os.path.isdir(abs_fakepath):
-                    os.mkdir(abs_fakepath)
+
                 ## make all links
                 ## they are create with relative paths !
 
@@ -107,21 +133,34 @@ class vsc_install_scripts(distutils.command.install_scripts.install_scripts):
                 ## abspath: all_syms.append(os.path.join(abs_fakepath, 'mpirun'))
                 ## with relative paths, we also ne to chdir for the fake/mpirun and ref to ../mympirun
                 previous_pwd = os.getcwd()
-                os.chdir(rel_script_dir)
 
+                os.chdir(rel_script_dir)
                 for sym_name in MYMPIRUN_ALIASES:
                     if os.path.exists(sym_name):
                         os.remove(sym_name)
                     os.symlink(rel_script, sym_name)
-                    self.outfiles.append(os.path.join(rel_script_dir, sym_name))
+                    newoutfile = os.path.join(rel_script_dir, sym_name)
+                    self.outfiles.append(newoutfile)
+                    log.info("symlink %s to %s newoutfile %s" % (rel_script, sym_name, newoutfile))
 
                 ## fake mpirun
-                os.chdir(abs_fakepath)
+                os.chdir(previous_pwd)
+                abs_fakepath = os.path.join(self.install_dir, FAKE_SUBDIRECTORY_NAME)
+                if not os.path.isdir(abs_fakepath):
+                    log.info("creating abs_fakepath %s" % abs_fakepath)
+                    os.mkdir(abs_fakepath)
+                else:
+                    log.info("not creating abs_fakepath %s (already exists)" % abs_fakepath)
+
+                os.chdir(abs_fakepath) ## abs_fakepath si not always absolute
                 fake_mpirun = os.path.join(abs_fakepath, 'mpirun')
                 if os.path.exists(fake_mpirun):
                     os.remove(fake_mpirun)
-                os.symlink('../%s' % rel_script, 'mpirun')
+
+                mympirun_src = '../%s' % rel_script
+                os.symlink(mympirun_src, 'mpirun')
                 self.outfiles.append(fake_mpirun)
+                log.info("symlink %s to %s newoutfile %s" % (mympirun_src, 'mpirun', fake_mpirun))
 
                 os.chdir(previous_pwd)
 
@@ -138,7 +177,8 @@ lm = ('Luis Fernando Munoz Meji­as', 'luis.munoz@ugent.be')
 SHARED_TARGET = {'url':'http://hpcugent.github.com/VSC-tools',
                  'download_url':'https://github.com/hpcugent/VSC-tools',
                  'package_dir' : {'': 'lib'},
-                 'cmdclass' : {"install_scripts": vsc_install_scripts}
+                 'cmdclass' : {"install_scripts": vsc_install_scripts,
+                               "easy_install":vsc_easy_install}
                  }
 
 ## meta-package for allinone target
@@ -169,7 +209,6 @@ VSC_MYMPIRUN = {'name':'vsc-mympirun',
 ###
 ### BUILDING
 ###
-from distutils.core import setup
 import sys
 
 def parse_target(target):
