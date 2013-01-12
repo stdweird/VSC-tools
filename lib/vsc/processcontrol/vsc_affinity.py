450dcdd8214bb67a89a60e94ed20adc91442992f
stdweird <stijn.deweirdt@ugent.be>
2013-01-12 20:34:09 +0100

    add implementation based on vsc.util.affinity

--- lib/vsc/processcontrol/vsc_affinity.py
+++ lib/vsc/processcontrol/vsc_affinity.py
@@ -29,11 +29,65 @@ Implement Priority and Affinity classes using vsc.utils.affinity
 
 from .priority import Priority
 from .affinity import Affinity
-from vsc.utils.affinity import sched_getaffinity, sched_setaffinity, cpu_set_t, getpriority, setpriority
+from .algorithm import BasicCore
+try:
+    from vsc.utils.affinity import sched_getaffinity, sched_setaffinity, cpu_set_t, getpriority, setpriority
+    valid = True
+except:
+    vaild = False
+
+MODE_NAME = 'vsc'
 
 class VSCPriority(Priority):
-    def get_priority(self):
-        return getpriority(who=self.pid)
+    PRIORITY_MODE = MODE_NAME
+    VALID = valid
+    def _get_priority(self):
+        self.priority = getpriority(who=self.pid)
 
-    def _set_priority(self,newprio):
-        setpriority(newprio, who=self.pid)
+    def _set_priority(self):
+        setpriority(self.priority, who=self.pid)
+
+class VSCAffinity(Affinity):
+    AFFINITY_MODE = MODE_NAME
+    VALID = valid
+    def _get_affinity(self):
+        ctypes_cpuset_t = sched_getaffinity(pid=self.pid)
+        self.cpusett.set_bits(cpus=ctypes_cpuset_t.cpus)
+
+    def _set_affinity(self):
+        cs = cpu_set_t()
+        cs.set_bits(cpus=self.cpusett.cpus)
+        sched_setaffinity(cs, pid=self.pid)
+
+class VSCAffinityBasic(VSCAffinity):
+    AFFINITY_ALGORITHM = 'basiccore'
+
+    def _algorithm(self, proc_nr, total_proc):
+        self.get_affinity()
+        allowed_cpus = self.cpusett.get_cpus_idx()  # takes into account cgroups/cpusets
+        total_cpus = len(allowed_cpus)
+
+        alg = BasicCore()
+        alg.create(total_cpus, total_proc)
+        rel_cpus = alg.get_cpus(proc_nr)
+
+        # rel_cpus is a list relative to the usable cpus of cpusett.cpus
+        for rel_idx, abs_idx in enumerate(allowed_cpus):
+            self.cpusett.cpus[abs_idx] = 1 if rel_idx in rel_cpus else 0
+
+if __name__ == '__main__':
+    import time
+    vp = VSCPriority()
+    print vp.get_priority()
+
+    vp.set_priority(5)
+    print vp.get_priority()
+
+    vab = VSCAffinityBasic()
+    print vab.get_affinity()
+    # process nr 1 out of 2 processes
+    vab.algorithm(1, 2)
+    print vab.get_affinity()
+
+
+    time.sleep(5)
